0d10b3671d664fd07badd0c59ae4b8d241baed27
Théo Pecqueux <tp@macbook-air.home>
2021-05-11 10:17:34 +0200

    ajout de la route delete/post/:id

---
+++ server/model/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required : true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    oauth: {
+        type: String,
+        required: true
+    },
+    followers: {
+        type: [String],
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: "online"
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
