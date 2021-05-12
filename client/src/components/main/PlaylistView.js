09c52e31ed93186f97843284b1e7cb5b31b6d157
Nicolas Wallet <nicolaswallet@hotmail.fr>
2021-05-12 11:12:16 +0200

    Using Moment in PlaylistView

--- client/src/components/main/PlaylistView.js
+++ client/src/components/main/PlaylistView.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { Playlist, PlaylistTracks } from "react-spotify-api";
+import Moment from "react-moment";
 
 class PlaylistView extends Component {
     constructor(props) {
@@ -16,15 +17,39 @@ class PlaylistView extends Component {
                 <PlaylistTracks id={this.state.id}>
                     {({ data }) =>
                         data
-                            ? data.items.map((track) => (
-                                  <div>
-                                      <img
-                                          src={track.track.album.images[0].url}
-                                          width="40px"
-                                      ></img>
-                                      <span key={track.track.id}>
+                            ? data.items.map((track, i) => (
+                                  <div className="row justify-content-between">
+                                      <div className="col">
+                                          <div class="d-flex align-items-center">
+                                              <div class="flex-grow-1 text-white-50">
+                                                  {i + 1}
+                                              </div>
+                                              <div class="flex-shrink-0">
+                                                  <img
+                                                      src={
+                                                          track.track.album
+                                                              .images[0].url
+                                                      }
+                                                      width="40px"
+                                                  ></img>
+                                              </div>
+                                          </div>
+                                      </div>
+
+                                      <div
+                                          className="col-5"
+                                          key={track.track.id}
+                                      >
                                           {track.track.name}
-                                      </span>
+                                      </div>
+                                      <div className="col text-white-50">
+                                          {track.track.artists[0].name}
+                                      </div>
+                                      <div className="col text-white-50 text-end">
+                                          <Moment format="m:ss">
+                                              {track.track.duration_ms}
+                                          </Moment>
+                                      </div>
                                   </div>
                               ))
                             : null
